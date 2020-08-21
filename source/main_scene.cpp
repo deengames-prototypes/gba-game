6db67e9a5e2b5d7b9382a76f1f096e2540c06eaa
nightblade9 <nightbladecodes@gmail.com>
2020-08-20 22:01:12 -0400

    User input detection/response + move lama Print keys bitflags on screen

--- source/main_scene.cpp
+++ source/main_scene.cpp
@@ -1,8 +1,14 @@
 #include <libgba-sprite-engine/sprites/sprite_builder.h>
+#include <libgba-sprite-engine/background/text_stream.h>
 
 #include "ff.h"
 #include "lama.h"
 #include "main_scene.h"
+
+const u16 UP_ARROW = 64;
+const u16 DOWN_ARROW = 128;
+const u16 LEFT_ARROW = 32;
+const u16 RIGHT_ARROW = 16;
 
 std::vector<Background *> MainScene::backgrounds() {
     return {};
@@ -27,4 +33,29 @@ void MainScene::load() {
 }
 
 void MainScene::tick(u16 keys) {
+    std::string keysMessage = std::string("Keys: ") + std::to_string(keys);
+    TextStream::instance().setText(keysMessage, 0, 0);
+
+    int vx = 0;
+    int vy = 0;
+
+    if ((keys & UP_ARROW) == UP_ARROW)
+    {
+        vy = -1;   
+    }
+    else if ((keys & DOWN_ARROW) == DOWN_ARROW)
+    {
+        vy = 1;
+    }
+    
+    if ((keys & LEFT_ARROW) == LEFT_ARROW)
+    {
+        vx = -1;   
+    }
+    else if ((keys & RIGHT_ARROW) == RIGHT_ARROW)
+    {
+        vx = 1;
+    }
+
+    lama->setVelocity(vx, vy);
 }
