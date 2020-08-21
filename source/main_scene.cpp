922eb3c68a9c439a8177e57723700bc44129b48a
nightblade9 <nightbladecodes@gmail.com>
2020-08-20 23:06:03 -0400

    Turn-based movement. Refactored arrow constants into MainScene. Lots of TODOs to improve stuff in the future. :/

--- source/main_scene.cpp
+++ source/main_scene.cpp
@@ -5,11 +5,6 @@
 #include "lama.h"
 #include "main_scene.h"
 
-const u16 UP_ARROW = 64;
-const u16 DOWN_ARROW = 128;
-const u16 LEFT_ARROW = 32;
-const u16 RIGHT_ARROW = 16;
-
 std::vector<Background *> MainScene::backgrounds() {
     return {};
 }
@@ -26,9 +21,10 @@ void MainScene::load() {
 
     lama = builder
             .withData(lamaTiles, sizeof(lamaTiles))
+            // TODO: turn into 16x16 flat colour
             .withSize(SIZE_32_32)
-            .withAnimated(6, 3)
-            .withLocation(50, 50)
+            .withAnimated(1, 1)
+            .withLocation(0, 0)
             .buildPtr();
 }
 
@@ -36,26 +32,43 @@ void MainScene::tick(u16 keys) {
     std::string keysMessage = std::string("Keys: ") + std::to_string(keys);
     TextStream::instance().setText(keysMessage, 0, 0);
 
-    int vx = 0;
-    int vy = 0;
-
-    if ((keys & UP_ARROW) == UP_ARROW)
+    if (justMoved && keys == 0)
     {
-        vy = -1;   
-    }
-    else if ((keys & DOWN_ARROW) == DOWN_ARROW)
-    {
-        vy = 1;
-    }
-    
-    if ((keys & LEFT_ARROW) == LEFT_ARROW)
-    {
-        vx = -1;   
-    }
-    else if ((keys & RIGHT_ARROW) == RIGHT_ARROW)
-    {
-        vx = 1;
+        // Nothing pressed ...
+        justMoved = false;
     }
 
-    lama->setVelocity(vx, vy);
+    if (!justMoved)
+    {
+        if ((keys & UP_ARROW) == UP_ARROW)
+        {
+            playerY -= 1;
+            if (playerY < 0)
+            {
+                playerY = TILES_HIGH - 1;
+            }
+            justMoved = true;
+        }
+        else if ((keys & DOWN_ARROW) == DOWN_ARROW)
+        {
+            playerY = (playerY + 1) % TILES_HIGH;
+            justMoved = true;
+        }
+        else if ((keys & LEFT_ARROW) == LEFT_ARROW)
+        {
+            playerX -= 1;
+            if (playerX < 0)
+            {
+                playerX = TILES_WIDE - 1;
+            }
+            justMoved = true;
+        }
+        else if ((keys & RIGHT_ARROW) == RIGHT_ARROW)
+        {
+            playerX = (playerX + 1) % TILES_WIDE;
+            justMoved = true;
+        }
+
+        lama->moveTo(playerX * TILE_SIZE, playerY * TILE_SIZE);
+    }
 }
--- source/main_scene.h
+++ source/main_scene.h
@@ -5,7 +5,25 @@
 
 class MainScene : public Scene {
 private:
+    const int TILE_SIZE = 16;
+    // TODO: move calculations somewhere for width/height/etc.
+    const int TILES_WIDE = 15;
+    const int TILES_HIGH = 10;
+
+    // Direction bitflags
+    const u16 UP_ARROW = 64;
+    const u16 DOWN_ARROW = 128;
+    const u16 LEFT_ARROW = 32;
+    const u16 RIGHT_ARROW = 16;
+
+    // TODO: extract out a class/lambda something like justPressed(key, lambda)
+    bool justMoved = false;
+
     std::unique_ptr<Sprite> lama;
+
+    // TODO: move this into a "model" class. With C++ tests?
+    int playerX;
+    int playerY;
 public:
     std::vector<Sprite *> sprites() override;
     std::vector<Background *> backgrounds() override;
