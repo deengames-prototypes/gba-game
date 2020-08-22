9ecef65bbe5cc2c018de1bcacbd7acc28f168127
3akev <3akev@protonmail.com>
2020-08-22 22:42:45 +0300

    Remove unused file

--- source/ff.h
+++
@@ -1,9 +0,0 @@
-#ifndef GBA_GAME_FF_H
-#define GBA_GAME_FF_H
-
-const unsigned short sharedPal[4] __attribute__((aligned(4))) __attribute__((visibility("hidden")))=
-{
-	0x18C6,0x6739,0x0C63,0x4210,
-};
-
-#endif //GBA_GAME_FF_H
